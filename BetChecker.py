f47cbbc0633bad83c280fede678414fe7af6e808
dirdam <adrian.jimenez@insightedge.jp>
2025-03-01 16:15:11 +0900

    Specify Chrome version

--- BetChecker.py
+++ BetChecker.py
@@ -38,7 +38,7 @@ class BetChecker():
             options.add_argument(
                 "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
             )
-        service = Service(ChromeDriverManager().install())
+        service = Service(ChromeDriverManager(version="120.0.6099.224").install())
         self.browser = webdriver.Chrome(service=service, options=options)
         self.browser.maximize_window()
 
