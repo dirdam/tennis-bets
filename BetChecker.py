8e9d34add04f8879993162fab35f669a8cdb87d3
dirdam <adrian.jimenez@insightedge.jp>
2025-03-01 15:52:43 +0900

    Add more headless arguments

--- BetChecker.py
+++ BetChecker.py
@@ -30,13 +30,14 @@ class BetChecker():
 
     def open_browser(self):
         options = Options()
-        options.add_argument('--disable-gpu')
         if self.headless:
             options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
             options.add_argument(
                 "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
             )
-        service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()) # Service(ChromeDriverManager().install())
+        service = Service(ChromeDriverManager().install())
         self.browser = webdriver.Chrome(service=service, options=options)
         self.browser.maximize_window()
 
