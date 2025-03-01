8a39b1a4018b12accbfe641300782e053e69b256
dirdam <adrian.jimenez@insightedge.jp>
2025-03-01 16:19:34 +0900

    Back to ChromeType

--- BetChecker.py
+++ BetChecker.py
@@ -30,16 +30,14 @@ class BetChecker():
 
     def open_browser(self):
         options = Options()
-        print('HHHH')
         if self.headless:
-            print('Headless mode activated')
             options.add_argument("--headless")
-            options.add_argument("--no-sandbox")
-            options.add_argument("--disable-dev-shm-usage")
+            # options.add_argument("--no-sandbox")
+            # options.add_argument("--disable-dev-shm-usage")
             options.add_argument(
                 "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
             )
-        service = Service(ChromeDriverManager(version="120.0.6099.224").install())
+        service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()) # Service(ChromeDriverManager().install())
         self.browser = webdriver.Chrome(service=service, options=options)
         self.browser.maximize_window()
 
