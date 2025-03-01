3220ae23168e4e0d123b2c68318e2505c8ab3b86
dirdam <adrian.jimenez@insightedge.jp>
2025-03-01 15:54:54 +0900

    Change requirements

--- BetChecker.py
+++ BetChecker.py
@@ -32,12 +32,10 @@ class BetChecker():
         options = Options()
         if self.headless:
             options.add_argument("--headless")
-            options.add_argument("--no-sandbox")
-            options.add_argument("--disable-dev-shm-usage")
             options.add_argument(
                 "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
             )
-        service = Service(ChromeDriverManager().install())
+        service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()) # Service(ChromeDriverManager().install())
         self.browser = webdriver.Chrome(service=service, options=options)
         self.browser.maximize_window()
 
