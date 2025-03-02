1774c692acc8914c4754ac10feb13cffd0c2aa53
dirdam <adrian.jimenez@insightedge.jp>
2025-03-02 14:40:03 +0900

    Add selenium options

--- BetChecker.py
+++ BetChecker.py
@@ -31,8 +31,9 @@ class BetChecker():
         options = Options()
         if self.headless:
             options.add_argument("--headless")
-            # options.add_argument("--no-sandbox")
-            # options.add_argument("--disable-dev-shm-usage")
+            options.add_argument('--disable-gpu')
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
             options.add_argument(
                 "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
             )
